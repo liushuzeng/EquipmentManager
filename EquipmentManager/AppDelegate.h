76744103528d61d02073f576e0f6f240173564fe
liushuzeng <liushuzeng@too.local>
2015-11-02 10:06:00 +0800

    Initial Commit

---
+++ EquipmentManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EquipmentManager
+//
+//  Created by liushuzeng on 15/11/2.
+//  Copyright © 2015年 liushuzeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EquipmentManager/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  EquipmentManager
+//
+//  Created by liushuzeng on 15/11/2.
+//  Copyright © 2015年 liushuzeng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
